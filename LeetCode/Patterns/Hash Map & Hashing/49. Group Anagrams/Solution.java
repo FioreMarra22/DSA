14198477ccd39ec48bf3cd96e659d6dcfc21d483
Fiore Marra <fiore.marra1996@gmail.com>
2026-09-05 00:23:14 -0500

    Add Medium Java Solution for 49. Group Anagrams from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/49. Group Anagrams/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/49. Group Anagrams/Solution.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //First thought is the use of a hashmap to store groups, Maybe sort through strings for a key 
-        HashMap<String, List<String>> group = new HashMap<>();
+        HashMap<String, List<String>> groupies= new HashMap<>();
 
         for (String s : strs){
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
-            //Sorted key
-            String sortingKey = new String(chars);
+            String key = new String(chars);
 
-            //Check then create 
-            if (!group.containsKey(sortingKey)) {
-                group.put(sortingKey, new ArrayList<>());
+            if (!groupies.containsKey(key)){
+               groupies.put(key, new ArrayList<>()); 
             }
-            group.get(sortingKey).add(s);
+            groupies.get(key).add(s);
         }
+        return new ArrayList<>(groupies.values());
 
-        return new ArrayList<>(group.values());
     }
 }
